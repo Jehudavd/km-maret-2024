034079764eff1b707068835bd2527fc5e585c5e7
Jehudavd <jehudavd@gmail.com>
2024-03-19 19:45:08 +0700

    latihan algoritma pseudocode

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # km-maret-2024
 kampus merdeka batch maret 2024
 
-kata kata hari ini 
+kata kata hari ini <br/>
 "terpaksa dipaksa terbiasa" <br/>
 "carilah error sebanyak mungkin"
---
+++ algoritmapseudocode.js
@@ -0,0 +1,18 @@
+// ALGORITMA PSEUDOCODE
+
+/**
+ *  STUDY CASE 1
+ * Tentukan algoritma dan pseudocode untuk menghitung keliling persegi panjang
+ * 
+ * 
+ * STUDY CASE 2
+ * Tentukan algoritma dan pseudocode untuk menghitung luas permukaan tabung
+ * 
+ * 
+ * STUDY CASE 3
+ * Tentukan algoritma dan pseudocode volume 1/4 volume bola
+ *  
+ */
+
+
+
