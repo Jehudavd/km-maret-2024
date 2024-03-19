bc89aea2fb52a49f66de136465c1025bfb7abd95
Jehudavd <jehudavd@gmail.com>
2024-03-19 21:29:52 +0700

    latihan variable tipe data

---
+++ basic-js/variabletipedata.js
@@ -0,0 +1,27 @@
+// var number = 10;
+// console.log(number);
+
+// var  number = 10;
+// console.log(number);
+
+// var str = "bola";
+// console.log(str);
+
+// var isCorrect = true;
+// console.log(isCorrect);
+
+// var a = 10;
+// var b = '5';
+// var total = a + b;
+// console.log(total);
+
+//typeof untuk cek tipe data
+//== membadingan value saja
+//=== membadingkan value dan tipe data
+
+var x = 10;
+var y = '10';
+
+var z = x  === y;
+console.log(z);
+
