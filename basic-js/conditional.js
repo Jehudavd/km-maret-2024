ad2d3e8dd65fae02585a37a9f510262638e8f25e
Jehudavd <jehudavd@gmail.com>
2024-03-20 21:04:24 +0700

    latihan conditional

---
+++ basic-js/conditional.js
@@ -0,0 +1,66 @@
+// CONDITONAL
+
+/**
+ * Study case 1
+ * 
+ * cek apakah bilangan ganjil atau genap
+ * 
+ * Buatlah algoritma, pseudocode dan coding
+ */
+
+/**
+ * ALGORITMA
+ * 1. Buat variable number
+ * 2. Cek jika number modulus 2 sama dengan 0, maka genap
+ * 3. Cek jika number modulus 2 sama dengan 1, maka ganjil
+ * 
+ * PSEUDOCODE
+ * 
+ * STORE number WITH ANY NUMBER
+ * 
+ * IF number MOD 2 EQUAL 0 THEN
+ * PRINT "Genap"
+ * ELSE
+ * PRINT "Ganjil"
+ * ENDIF 
+ * 
+ */
+
+var angka = 2;
+if (angka % 2 === 0){
+    console.log("Genap");
+}else{
+    console.log("Ganjil")
+}
+
+/**
+ * Study case 2
+ * 
+ * GRADE NILAI
+ * 
+ * Score 100 - 85, grade A
+ * Score 84 - 70, grade B
+ * Score 69 - 50. grade C
+ * Score 49 - 30, grade D
+ * Score 30 - 0, grade E
+ * 
+ * Buatlah pseudocode dan coding
+ */
+
+/**
+ * PSEUDOCODE
+ * 
+ * STORE score WITH ANY NUMBER
+ * 
+ * IF score GREATER THAN EQUAL 85 AND LESS THAN EQUAL 10 THEN
+ * PRINT "Grade A"
+ * ELSE IF score GREATER THAN EQUAL 70 AND LESS THAN 85 THEN 
+ * PRINT "Grade B"
+ * ELSE IF score GREATER THAN EQUAL 50 AND LESS THAN 70 THEN 
+ * PRINT "Grade C"
+ * ELSE IF score GREATER THAN EQUAL 30 AND LESS THAN 50 THEN 
+ * PRINT "Grade D"
+ * ELSE IF score GREATER THAN EQUAL 0 AND LESS THAN 30 THEN
+ * PRINT "Grade E"
+ */
+
