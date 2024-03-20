97967be6d4028bf605b44ee102e12b686971a55c
Jehudavd <jehudavd@gmail.com>
2024-03-20 21:08:49 +0700

    latihan conditional

--- basic-js/conditional.js
+++ basic-js/conditional.js
@@ -64,3 +64,19 @@ if (angka % 2 === 0){
  * PRINT "Grade E"
  */
 
+var score = 75;
+
+if (score >= 85 && score <= 100) {
+    console.log("Grade A");
+} else if (score >= 70 && score < 85) {
+    console.log("Grade B");
+} else if (score >= 50 && score < 70) {
+    console.log("Grade C");
+} else if (score >= 30 && score < 50) {
+    console.log("Grade D");
+} else if (score >= 0 && score < 30) {
+    console.log("Grade E");
+} else {
+    console.log("Invalid score");
+}
+
