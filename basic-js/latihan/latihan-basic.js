7d8bd0c0ce5997e3a8a36d7b21adda10bbf80491
Jehudavd <jehudavd@gmail.com>
2024-03-20 20:00:57 +0700

    latihan-basic-js

---
+++ basic-js/latihan/latihan-basic.js
@@ -0,0 +1,27 @@
+var umur = 17;
+if (umur >= 17){
+console.log("Bisa bikin ktp");
+}else{
+console.log("Belum bisa bikin ktp");
+}
+
+var minuman = "teh";
+switch(minuman){
+    case `kopi`:
+        console.log("Kamu pilih kopi");
+        break;
+    case `teh`:
+        console.log("Kamu pilih teh");
+        break;
+}
+
+var umur = 17;
+var tinggi = 155;
+
+if (umur < 17){
+    if (tinggi < 150){
+        console.log("tidak boleh masuk");
+    }if (tinggi >= 150){
+        console.log("boleh masuk");
+    }
+}
