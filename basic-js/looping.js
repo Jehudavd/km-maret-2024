a2758461aa525620b25ee33427af393e4a210386
Jehudavd <jehudavd@gmail.com>
2024-03-21 19:43:56 +0700

    belajar nested looping

--- basic-js/looping.js
+++ basic-js/looping.js
@@ -23,19 +23,19 @@
  */
 
 // Masukkan bilangan di sini
-const number = 23;
+// const number = 23;
 
-let count = 0;
+// let count = 0;
 
 // Loop dari 1 hingga bilangan itu sendiri
-for (let i = 1; i <= number; i++) {
-    // Jika bilangan dapat dibagi habis oleh i
-    if (number % i === 0) {
-        count++; // Tambahkan 1 ke jumlah faktor
-    }
-}
+// for (let i = 1; i <= number; i++) {
+//     // Jika bilangan dapat dibagi habis oleh i
+//     if (number % i === 0) {
+//         count++; // Tambahkan 1 ke jumlah faktor
+//     }
+// }
 
-console.log(count);
+// console.log(count);
 
 
 /**
@@ -44,8 +44,83 @@ console.log(count);
  * Cek apakah bilangan prima
  */
 
-if(count === 2){
-    console.log("Bilangan prima");
-}else{
-    console.log("Bukan bilangan prima");
+// if(count === 2){
+//     console.log("Bilangan prima");
+// }else{
+//     console.log("Bukan bilangan prima");
+// }
+
+/**
+ * Study case 4
+ * 
+ * Tampilkan bentuk segitiga siku siku
+ * 
+ * #
+ * ##
+ * ###
+ * ####
+ * #####
+ * 
+ */
+
+// Variabel untuk tinggi segitiga
+var tinggi = 0;
+
+// Loop untuk menghasilkan setiap baris
+for (var i = 1; i <= tinggi; i++) {
+    var baris = '';
+    // Loop untuk menambahkan '#' sesuai dengan nomor baris
+    for (var j = 1; j <= i; j++) {
+        baris += '#';
+    }
+    // Menampilkan baris yang telah dibuat
+    console.log(baris);
+}
+
+var n = 5;
+var result = "";
+
+for(var i = 0; i < n; i++){
+    for(var j = 0; j <= i; j++){
+        result += '#';
+    }
+    console.log(result);
+    result = "";
+}
+
+/**
+ * Study case 5
+ * 
+ * Tampilkan angka dalam bentuk segitiga siku siku
+ * 
+ * 1
+ * 12
+ * 123
+ * 1234
+ * 12345
+ */
+
+// Variabel untuk tinggi segitiga
+var tinggi = 0;
+
+// Loop untuk menghasilkan setiap baris
+for (var i = 1; i <= tinggi; i++) {
+    var baris = '';
+    // Loop untuk menambahkan angka dari 1 hingga i pada setiap baris
+    for (var j = 1; j <= i; j++) {
+        baris += j;
+    }
+    // Menampilkan baris yang telah dibuat
+    console.log(baris);
+}
+
+var x = 5;
+var hasil = "";
+
+for(var i = 0; i < x; i++){
+    for(var j = 1; j <= i+1; j++){
+        hasil += j;
+    }
+    console.log(hasil);
+    hasil = "";
 }
