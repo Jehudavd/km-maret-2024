cd68bb4cb179b07817944a77f69efa84efed667a
Jehudavd <jehudavd@gmail.com>
2024-03-28 20:59:41 +0700

    belajar factory class

---
+++ oop-mvc/oop/factoryclass.js
@@ -0,0 +1,58 @@
+class Person {
+    constructor(name, job, isGraduated, city, skills){
+        this.name = name;
+        this.job = job;
+        this.isGraduated = isGraduated;
+        this.city = city;
+        this.skills = skills;
+    }
+}
+
+class Developer extends Person {
+    constructor(name, isGraduated, city, skills, status){
+        super(name, "developer", isGraduated, city, skills);
+        this.status = status || "";
+    }
+}
+
+class Designer extends Person {
+    constructor(name, isGraduated, city, skills, tools){
+        super(name, "designer", isGraduated, city, skills);
+        this.tools = tools;
+    }
+}
+
+let developer = new Developer("yuda", true, "medan", ["js", "css", "masih sendiri"])
+let designer = new Designer("agung", false, "maluku", [], "figma")
+
+// console.log(developer);
+// console.log(designer);
+
+class Office{
+    constructor(rooms){
+        this.rooms = rooms || [];
+    }
+
+    showStaffs(){
+
+    }
+
+    addStaff(name, job, isGraduated, city, skills){
+        switch(job){
+            case `developer`:
+                this.rooms.push(new Developer(name, isGraduated, city, skills))
+                break;
+            case "designer":
+                this.rooms.push(new Designer(name, isGraduated, city, skills))
+                break;
+        }
+    }
+}
+
+const office = new Office();
+
+office.addStaff("yuda", "developer", true, "medan", ["js", "css"]);
+office.addStaff("david", "designer", false, "manado", []);
+
+console.log(office);
+
